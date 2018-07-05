95f77d681a5f010ca3275be2c60a9f96f2bd8197
Gilbert <94gagb@gmail.com>
2018-07-05 18:25:28 -0400

    codigo para detectar error por timeout y busqueda de replica V1

--- Cliente/src/comunicacion/Conexion_Anillo.java
+++ Cliente/src/comunicacion/Conexion_Anillo.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import peticiones.Mensaje;
@@ -28,10 +29,12 @@ public class Conexion_Anillo {
     public String peticionAnillo( Mensaje mensaje ){
         String nombre;
         try {
-            Socket peticionCentral = new Socket( "192.168.1.104", 11000 );
+            Socket peticionCentral = new Socket( "localhost", 11000 );
             System.out.println( "Realizando petición de entrada al anillo" );
             ObjectOutputStream oos = new ObjectOutputStream( peticionCentral.getOutputStream() );
             ObjectInputStream ois = new ObjectInputStream( peticionCentral.getInputStream() );
+            //Descomentar para probar el error de comunicacion.
+            //peticionCentral.setSoTimeout(1);
             oos.writeObject(mensaje);
             oos.flush();
             Mensaje respuesta = new Mensaje();
@@ -40,13 +43,26 @@ public class Conexion_Anillo {
             oos.close();
             ois.close();
             nombre = respuesta.getMensaje();
-        } catch (IOException ex) {
+        }
+        /*
+        Descomentar para probar el error de comunicacion.
+        
+        catch (SocketTimeoutException ex) {
+            Logger.getLogger(Conexion_Anillo.class.getName()).log(Level.SEVERE, null, ex);
+            //Hay que leer el json que se recibirá en algún momento con la info
+            //de los otros servidores.
+            System.out.println("Error en comunicación buscando otro servidor, por favor espere.");
+            nombre = "500";
+        }
+        */catch (IOException ex) {
             Logger.getLogger(Conexion_Anillo.class.getName()).log(Level.SEVERE, null, ex);
             nombre = "500";
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Conexion_Anillo.class.getName()).log(Level.SEVERE, null, ex);
             nombre = "500";
         }
+        
+        
         return nombre;
     }
     
